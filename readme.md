74e7be175425d5f58a5e5b0fa19d248c7b94ab2e
Gustavo Machado <machadogj@gmail.com>
2012-07-20 13:27:57 -0300

    initial commit

---
+++ readme.md
@@ -0,0 +1,55 @@
+Access Google BigQuery using a service account from node.js
+
+
+
+# Usage
+For the sake of simplicity, the entities returned by Google's webservice are being used, so you can refer to Google's site https://developers.google.com/bigquery/docs/reference/v2/ for further documentation.
+For the entire usage of the API you can go see the test\tests.js file, here are some samples:
+
+```js
+
+var bigquery = require('google-bigquery'),
+	client = bigquery({
+		"iss": '1234567890@developer.gserviceaccount.com',
+		"key": fs.readFileSync('mypemfile.pem', 'utf8')
+	});
+
+
+	client.getProjects(function (err, projs) {
+		console.log(projs);
+		console.log(projs.projects); //list of projects.
+	});
+
+	client.tables.create({... your table resource ...}, function (err, table){
+		console.log(table);
+		console.log(table.tableReference.tableId); //table's id.
+	});
+
+```
+
+# Tests
+In order to execute the tests, first set up an environment variable 'GOOGLE_ISS' with the ISS taken from your Google API Service Account (notice that you have to register your Google API account for BigQuery first), something like this: "abcdefghij@developer.gserviceaccount.com".
+You also have to get a .pem with your private key to sign the token requests. To get this .pem file, create a service account in Google API's console, and download the .p12 file. In linux you can convert the .p12 file with openssl using the following command (notice the -nodes):
+
+openssl -in yourp12file.p12 -out file.pem -nodes
+
+Copy your .pem file to the 'test' folder, and then run the tests using mocha or npm test.
+
+Notice: these tests are going to be hitting your bigquery account directly, so I recommend setting a tests project, and setting the mocha timeout to 10 seconds (mocha --timeout 10000 test/tests.js).
+
+# TODO
+* Uploading of data into tables.
+* Better API for querying tables (right now you do it through jobs create and query methods)
+* Review the token expiration/refresh logic.
+
+# License
+
+The MIT License : http://opensource.org/licenses/MIT
+
+Copyright (c) 2011-2012 Gustavo Machado. http://machadogj.com
+
+Permission is hereby granted, free of charge, to any person obtaining a copy of this software and associated documentation files (the 'Software'), to deal in the Software without restriction, including without limitation the rights to use, copy, modify, merge, publish, distribute, sublicense, and/or sell copies of the Software, and to permit persons to whom the Software is furnished to do so, subject to the following conditions:
+
+The above copyright notice and this permission notice shall be included in all copies or substantial portions of the Software.
+
+THE SOFTWARE IS PROVIDED 'AS IS', WITHOUT WARRANTY OF ANY KIND, EXPRESS OR IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY, FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM, OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE SOFTWARE.
---
+++ test/tests.js
@@ -0,0 +1,295 @@
+var assert = require('assert'),
+	bigquery = require('../lib'),
+	fs = require('fs'),
+	pem = fs.readFileSync(__dirname + '/file.pem', 'utf8'),
+	client = bigquery({
+		"iss": process.env['GOOGLE_ISS'],
+		"key": pem
+	});
+
+describe('google bigquery client', function (){
+
+	it('should throw error if iss is not defined', function (){
+		var throwed = false;
+		
+		try { bigquery({"key":"foo"}); }
+		catch(err) { throwed = true; }
+		
+		assert.ok(throwed);
+	});
+
+	it('should throw error if key is not defined', function () {
+		var throwed = false;
+		
+		try { bigquery({"iss":"foo"}); }
+		catch(err) { throwed = true; }
+		
+		assert.ok(throwed);
+	});
+
+	describe('projects', function (){
+
+		it('should query for projects', function (done) {
+			client.getProjects(function (err, projs) {
+				assert.equal(undefined, err);
+				assert.ok(projs);
+				assert.ok(projs.projects.length > 0);
+				done();
+			});
+		});
+
+	});
+
+	describe('datasets', function () {
+
+		it('should create a dataset', function (done){
+			var name = "test" + ~~(Math.random() * 100000);
+			client.getProjects(function (err, projs) {
+				var projId = projs.projects[0].id;
+				client.datasets.create(name, projId, function (err, dataset) {
+					assert.equal(undefined, err);
+					assert.ok(dataset);
+					assert.equal(name, dataset.friendlyName);
+					//try to clean up the mess but don't blow the test.
+					client.datasets.delete(dataset.datasetReference.datasetId, projId, true, function (err) {});
+					done();
+				});
+			});
+		});
+
+		it('should delete a dataset', function (done){ 
+			var name = "test" + ~~(Math.random() * 100000);
+			client.getProjects(function (err, projs) {
+				var projId = projs.projects[0].id;
+				client.datasets.create(name, projId, function (err, dataset) {
+					client.datasets.delete(dataset.datasetReference.datasetId, projId, true, function (err) {
+						assert.equal(undefined, err);
+						done();
+					});
+				});
+			});
+		});
+
+		it('should query for datasets', function (done) {
+			client.getProjects(function (err, projs) {
+				client.datasets.getAll(projs.projects[0].id, function (err, datasets) {
+					assert.equal(undefined, err);
+					assert.ok(datasets);
+					assert.ok(datasets.datasets.length > 0);
+					done();
+				});
+			});
+		});
+
+		it('should get a dataset', function (done) {
+			client.getProjects(function (err, projs) {
+				var projId = projs.projects[0].id;
+				client.datasets.getAll(projId, function (err, datasets) {
+					//get the first.
+					var first = datasets.datasets[0].datasetReference.datasetId;
+					client.datasets.get(first, projId, function (err, dataset) {
+						assert.equal(undefined, err);
+						assert.ok(dataset);
+						assert.equal(first, dataset.datasetReference.datasetId);
+						done();
+					});
+				});
+			});
+		});
+
+	});
+
+	describe('tables', function () {
+
+		it('should create a table', function ( done ){ 
+			client.getProjects(function (err, projs) {
+				var projId = projs.projects[0].id;
+				client.datasets.getAll(projId, function (err, datasets) {
+					//get the first.
+					var first = datasets.datasets[0].datasetReference.datasetId,
+						name = "tesTable" + ~~(Math.random() * 100000),
+						table = {
+							friendlyName: name,
+							tableReference: {
+								projectId: projId,
+								datasetId: first,
+								tableId: name
+							},
+							schema: {
+								fields: [
+									{ name: 'col1', type: 'STRING' }
+								]
+							}
+						};
+					
+					client.tables.create(table, function (err, result) {
+						assert.equal(undefined, err);
+						assert.ok(result);
+						assert.equal(name, result.tableReference.tableId);
+						done();
+						//clean up table without blowing up the test
+						client.tables.delete(name, first, projId, function (){});
+					});
+				});
+			});
+		});
+
+		it('it should delete a table', function ( done ) { 
+			client.getProjects(function (err, projs) {
+				var projId = projs.projects[0].id;
+				client.datasets.getAll(projId, function (err, datasets) {
+					//get the first.
+					var first = datasets.datasets[0].datasetReference.datasetId,
+						name = "tesTable" + ~~(Math.random() * 100000),
+						table = {
+							friendlyName: name,
+							tableReference: {
+								projectId: projId,
+								datasetId: first,
+								tableId: name
+							},
+							schema: {
+								fields: [
+									{ name: 'col1', type: 'STRING' }
+								]
+							}
+						};
+					
+					client.tables.create(table, function (err, result) {
+						client.tables.delete(name, first, projId, function (err) {
+							assert.equal(undefined, err);
+							done();
+						});
+					});
+				});
+			});
+		});
+
+		it('it should list all tables', function ( done ) { 
+			client.getProjects(function (err, projs) {
+				var projId = projs.projects[0].id;
+				client.datasets.getAll(projId, function (err, datasets) {
+					//get the first.
+					var first = datasets.datasets[0].datasetReference.datasetId;
+					console.log(first);
+					client.tables.getAll(first, projId, function (err, tables) {
+						assert.equal(undefined, err);
+						assert.ok(tables.tables.length > 0);
+						done();
+					});
+				});
+			});
+		});
+
+		it('it should get a table', function ( done ) {
+			client.getProjects(function (err, projs) {
+				var projId = projs.projects[0].id;
+				client.datasets.getAll(projId, function (err, datasets) {
+					//get the first.
+					var first = datasets.datasets[0].datasetReference.datasetId;
+					console.log(first);
+					client.tables.getAll(first, projId, function (err, tables) {
+						var tableId = tables.tables[0].tableReference.tableId;
+						client.tables.get(tableId, first, projId, function ( err, table ) {
+							assert.equal(undefined, err);
+							assert.ok(table);
+							assert.equal(tableId, table.tableReference.tableId);
+							done();
+						});
+					});
+				});
+			});
+		});
+
+	});
+
+	describe('jobs', function () {
+
+		it('should create a job', function (done){
+			var name = "testTable" + ~~(Math.random() * 100000),
+				destinationTable = "destTable" + ~~(Math.random() * 100000),
+				jobName = "testJob" + ~~(Math.random() * 100000);
+			client.getProjects(function (err, projs) {
+
+				var projId = projs.projects[0].id;
+				client.datasets.getAll(projId, function (err, datasets) {
+					//get the first.
+					var first = datasets.datasets[0].datasetReference.datasetId,
+						table = {
+							friendlyName: name,
+							tableReference: {
+								projectId: projId,
+								datasetId: first,
+								tableId: name
+							},
+							schema: {
+								fields: [
+									{ name: 'col1', type: 'STRING' }
+								]
+							}
+						};
+
+					client.tables.create(table, function (err, result) {
+						
+						var job = {
+							id: jobName,
+							jobReference: {
+								projectId: projId,
+								jobId: jobName
+							},
+							configuration: {
+								copy: {
+									sourceTable:{
+										projectId: projId,
+										datasetId: first,
+										tableId: name
+									},
+									destinationTable: {
+										projectId: projId,
+										datasetId: first,
+										tableId: destinationTable
+									},
+									createDisposition: 'CREATE_IF_NEEDED',
+									writeDisposition: 'WRITE_TRUNCATE'
+								}
+							}
+						};
+						client.jobs.create(job, function (err, entity) {
+							assert.equal(undefined, err);
+							assert.ok(entity);
+							done();
+						});
+					});
+				});
+			});
+		});
+
+		it('should query for jobs', function (done) {
+			client.getProjects(function (err, projs) {
+				client.jobs.getAll(projs.projects[0].id, function (err, jobs) {
+					assert.equal(undefined, err);
+					assert.ok(jobs);
+					assert.ok(jobs.jobs.length > 0);
+					done();
+				});
+			});
+		});
+
+		it('should get a job', function (done) {
+			client.getProjects(function (err, projs) {
+				var projId = projs.projects[0].id;
+				client.jobs.getAll(projId, function (err, jobs) {
+					//get the first.
+					var first = jobs.jobs[0].jobReference.jobId;
+					client.jobs.get(first, projId, function (err, entity) {
+						assert.equal(undefined, err);
+						assert.ok(entity);
+						assert.equal(first, entity.jobReference.jobId);
+						done();
+					});
+				});
+			});
+		});
+
+	});
+});
